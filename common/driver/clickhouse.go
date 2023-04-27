017fda855ea021078848cd9b94c87a371411df0c
sunhongtao <sunhongtao@xiaohulu.com>
2023-04-27 17:43:16 +0800

    fix:解决数据库启动缓慢导致连接失败的问题

--- common/driver/clickhouse.go
+++ common/driver/clickhouse.go
@@ -13,45 +13,58 @@ import (
 
 func OpenCK(user, password, addr, dbName string, port int, xlog *xlog.XLog) (*gorm.DB, error) {
 
-	//mysql
-	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4,utf8&parseTime=True&loc=%s",
-	//	user,
-	//	password,
-	//	addr,
-	//	port,
-	//	dbName,
-	//	"Asia%2FShanghai")
-	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	//PostgresSQL
-	//dsn := "host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Shanghai"
-	//dsn = fmt.Sprintf(dsn, addr, user, password, dbName, port)
+	var err error
+	var db *gorm.DB
+	var count int64
 
-	dsn := "clickhouse://%v:%v@%v:%v/%v"
-	dsn = fmt.Sprintf(dsn, user, password, addr, port, dbName)
+	for true {
 
-	//dsn = fmt.Sprintf(dsn, addr, port, dbName, user, password)
+		//mysql
+		//dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4,utf8&parseTime=True&loc=%s",
+		//	user,
+		//	password,
+		//	addr,
+		//	port,
+		//	dbName,
+		//	"Asia%2FShanghai")
+		//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+		//PostgresSQL
+		//dsn := "host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Shanghai"
+		//dsn = fmt.Sprintf(dsn, addr, user, password, dbName, port)
 
-	l := logger.New(log.New(xlog.Out, "\r\n", log.LstdFlags), logger.Config{
-		SlowThreshold:             200 * time.Millisecond,
-		LogLevel:                  logger.LogLevel(xlog.Level),
-		IgnoreRecordNotFoundError: false,
-		Colorful:                  true,
-	})
+		dsn := "clickhouse://%v:%v@%v:%v/%v"
+		dsn = fmt.Sprintf(dsn, user, password, addr, port, dbName)
 
-	db, err := gorm.Open(clickhouse.Open(dsn), &gorm.Config{
-		Logger: l,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
-	if err != nil {
-		return nil, err
+		//dsn = fmt.Sprintf(dsn, addr, port, dbName, user, password)
+
+		l := logger.New(log.New(xlog.Out, "\r\n", log.LstdFlags), logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  logger.LogLevel(xlog.Level),
+			IgnoreRecordNotFoundError: false,
+			Colorful:                  true,
+		})
+
+		db, err = gorm.Open(clickhouse.Open(dsn), &gorm.Config{
+			Logger: l,
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+		})
+		if err != nil && count < 3 {
+			count++
+			time.Sleep(10 * time.Second)
+			continue
+		} else if err != nil && count >= 3 {
+			break
+		}
+
+		d, _ := db.DB()
+		d.SetMaxOpenConns(30)
+		d.SetConnMaxLifetime(7 * time.Hour)
+		d.SetMaxIdleConns(20)
+		d.SetConnMaxIdleTime(70 * time.Minute)
+		return db, nil
 	}
 
-	d, _ := db.DB()
-	d.SetMaxOpenConns(30)
-	d.SetConnMaxLifetime(7 * time.Hour)
-	d.SetMaxIdleConns(20)
-	d.SetConnMaxIdleTime(70 * time.Minute)
-	return db, nil
+	return nil, err
 }

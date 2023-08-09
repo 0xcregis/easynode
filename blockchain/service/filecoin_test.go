09b215bd53fe50775dee33dcd5e62cfd66ede1dd
sunhongtao <sunhongtao@xiaohulu.com>
2023-08-09 14:41:52 +0800

    test(blockchain): update filecoin_test

--- blockchain/service/filecoin_test.go
+++ blockchain/service/filecoin_test.go
@@ -26,7 +26,7 @@ func TestFileCoin_GetLatestBlock(t *testing.T) {
 
 func TestFileCoin_GetBlockByNumber(t *testing.T) {
 	s := Init5()
-	resp, err := s.GetBlockByNumber(301, "3091306", false)
+	resp, err := s.GetBlockByNumber(301, "3106211", false)
 	if err != nil {
 		t.Error(err)
 	} else {
@@ -36,7 +36,7 @@ func TestFileCoin_GetBlockByNumber(t *testing.T) {
 
 func TestFileCoin_GetBlockByHash(t *testing.T) {
 	s := Init5()
-	resp, err := s.GetBlockByHash(301, "bafy2bzacebll5xugzbbrfvyzrzvn25rvdschatyuw2bharrgx3tt6qgb7cag2", false)
+	resp, err := s.GetBlockByHash(301, "bafy2bzacecs5veov5flezd6eol7ezbnwrjr36jkym5q4i7yfbne5nnubctrps", true)
 	if err != nil {
 		t.Error(err)
 	} else {
@@ -56,13 +56,24 @@ func TestFileCoin_Balance(t *testing.T) {
 
 func TestFileCoin_GetTxByHash(t *testing.T) {
 	s := Init5()
-	resp, err := s.GetTxByHash(301, "bafy2bzacebttywwub67mp65nmovcb5axtas7luvfqhl7gay3pwlplfenjttwc")
+	resp, err := s.GetTxByHash(301, "bafy2bzacea32eakhkxtezwm2ura2ooevpbqhjzkt4maeib2ldz52x6rjfqzqo")
 	if err != nil {
 		t.Error(err)
 	} else {
 		t.Log(resp)
 	}
 }
+
+//func TestFileCoin_GetTxsByHash(t *testing.T) {
+//	s := Init5()
+//	resp, err := s.GetTxsByHash(301, "bafy2bzacecs5veov5flezd6eol7ezbnwrjr36jkym5q4i7yfbne5nnubctrps")
+//	if err != nil {
+//		t.Error(err)
+//	} else {
+//		t.Log(resp)
+//	}
+//
+//}
 
 func TestFileCoin_GetBlockReceiptByBlockNumber(t *testing.T) {
 	s := Init5()
@@ -86,37 +97,48 @@ func TestFileCoin_GetTransactionReceiptByHash(t *testing.T) {
 }
 
 func TestFileCoin_SendRawTransaction(t *testing.T) {
-	s := Init5()
-	tx := `
-  {
-    "Message": {
-      "Version": 42,
-      "To": "f01234",
-      "From": "f01234",
-      "Nonce": 42,
-      "Value": "0",
-      "GasLimit": 9,
-      "GasFeeCap": "0",
-      "GasPremium": "0",
-      "Method": 1,
-      "Params": "Ynl0ZSBhcnJheQ==",
-      "CID": {
-        "/": "bafy2bzacebbpdegvr3i4cosewthysg5xkxpqfn2wfcz6mv2hmoktwbdxkax4s"
-      }
-    },
-    "Signature": {
-      "Type": 2,
-      "Data": "Ynl0ZSBhcnJheQ=="
-    },
-    "CID": {
-      "/": "bafy2bzacebbpdegvr3i4cosewthysg5xkxpqfn2wfcz6mv2hmoktwbdxkax4s"
-    }
-  }
-`
-	t.Log(s.SendRawTransaction(301, tx))
+	/*	s := Init5()
+			tx := `
+		  {
+		    "Message": {
+		      "Version": 42,
+		      "To": "f01234",
+		      "From": "f01234",
+		      "Nonce": 42,
+		      "Value": "0",
+		      "GasLimit": 9,
+		      "GasFeeCap": "0",
+		      "GasPremium": "0",
+		      "Method": 1,
+		      "Params": "Ynl0ZSBhcnJheQ==",
+		      "CID": {
+		        "/": "bafy2bzacebbpdegvr3i4cosewthysg5xkxpqfn2wfcz6mv2hmoktwbdxkax4s"
+		      }
+		    },
+		    "Signature": {
+		      "Type": 2,
+		      "Data": "Ynl0ZSBhcnJheQ=="
+		    },
+		    "CID": {
+		      "/": "bafy2bzacebbpdegvr3i4cosewthysg5xkxpqfn2wfcz6mv2hmoktwbdxkax4s"
+		    }
+		  }
+		`
+			r,err:=s.SendRawTransaction(301, tx)
+			if err != nil {
+				t.Error(err)
+			} else {
+				t.Log(r)
+			}*/
+
 }
 
 func TestFileCoin_Nonce(t *testing.T) {
 	s := Init5()
-	t.Log(s.Nonce(301, "f1ys5qqiciehcml3sp764ymbbytfn3qoar5fo3iwy", ""))
+	r, err := s.Nonce(301, "f1ys5qqiciehcml3sp764ymbbytfn3qoar5fo3iwy", "")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(r)
+	}
 }

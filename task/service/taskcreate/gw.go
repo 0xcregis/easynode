e0b00ae694d5176e90002858e01c094d3021c398
sunhongtao <sunhongtao@xiaohulu.com>
2023-10-11 16:23:17 +0800

    feat(task): add bnb for easynode

--- task/service/taskcreate/gw.go
+++ task/service/taskcreate/gw.go
@@ -3,6 +3,7 @@ package taskcreate
 import (
 	"github.com/0xcregis/easynode/task"
 	"github.com/0xcregis/easynode/task/config"
+	"github.com/0xcregis/easynode/task/service/taskcreate/bnb"
 	"github.com/0xcregis/easynode/task/service/taskcreate/btc"
 	"github.com/0xcregis/easynode/task/service/taskcreate/ether"
 	"github.com/0xcregis/easynode/task/service/taskcreate/filecoin"
@@ -19,6 +20,8 @@ func NewApi(blockchain int64, log *xlog.XLog, v *config.BlockConfig) task.BlockC
 		return tron.NewTron(log, v)
 	} else if blockchain == 201 {
 		return polygonpos.NewPolygonPos(log, v)
+	} else if blockchain == 202 {
+		return bnb.NewBnb(log, v)
 	} else if blockchain == 301 {
 		return filecoin.NewFileCoin(log, v)
 	} else if blockchain == 310 {

8054af0f2aca8b297c928ab7ac2259f500a0409e
sunhongtao <sunhongtao@xiaohulu.com>
2023-12-01 10:53:32 +0800

    feat: refact config file for blockchain

--- blockchain/config/configutil.go
+++ blockchain/config/configutil.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"os"
+
+	"github.com/tidwall/gjson"
 )
 
 func LoadConfig(path string) Config {
@@ -19,6 +21,30 @@ func LoadConfig(path string) Config {
 		panic(err)
 	}
 	cfg := Config{}
-	_ = json.Unmarshal(b, &cfg)
+	err = json.Unmarshal(b, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	list := gjson.ParseBytes(b).Get("Nodes").Array()
+	for _, v := range list {
+		chainCode := v.Get("BlockChain").Int()
+		var node NodeCluster
+		err := json.Unmarshal([]byte(v.String()), &node)
+		if err != nil {
+			panic(err)
+		}
+
+		if m, ok := cfg.Cluster[chainCode]; ok {
+			m = append(m, &node)
+			cfg.Cluster[chainCode] = m
+		} else {
+			m := make(map[int64][]*NodeCluster, 1)
+			nodeList := []*NodeCluster{&node}
+			m[chainCode] = nodeList
+			cfg.Cluster = m
+		}
+	}
+
 	return cfg
 }
